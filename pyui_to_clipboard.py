5a3eb3b15c120771fbf60dd0ad589eccde0714a9
Tutorial Doctor <TutorialDoctor@users.noreply.github.com>
2015-08-05 16:02:19 -0400

    Update pyui_to_clipboard.py
    Added error handling.
    Gets the UI file of the current script. 
    The regular expression needs a bit of work though.

--- pyui_to_clipboard.py
+++ pyui_to_clipboard.py
@@ -1,5 +1,30 @@
+#Copy pyui to clipboard
+"""
+	- Now gets the pyui file of the current file. Can be added as an action.
+	- Error handling if ui file is not associated with current file.
+"""
+
 import clipboard
-filename = 'put_your_filename_here.pyui'  # edit this line before running
-with open(filename) as in_file:
-    clipboard.set(in_file.read())
-print('The contents of {} are now on the clipboard.'.format(filename))
+import editor
+import re
+
+
+file_path = editor.get_path()
+pattern = "\w+\s*\b*\w+\.py"
+name = re.findall(pattern,file_path)
+
+
+filename= ''.join(name)
+ui_filename = filename+'ui'
+
+
+try:
+	with open(ui_filename) as in_file:
+		clipboard.set(in_file.read())
+	print('contents of {} are on the clipboard'.format(ui_filename))
+except IOError:
+	print 'The file {} has no associated UI file to add to the clipboard.'.format(filename)
+	print ui_filename
+	
+
+# The regular expression needs work. doesnt work for single letter names.
