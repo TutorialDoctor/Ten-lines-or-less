283c7a5eb85787d09230b3d8b1151bb9b6e88a3b
Tutorial Doctor <TutorialDoctor@users.noreply.github.com>
2015-08-05 16:10:45 -0400

    Update pyui_to_clipboard.py

--- pyui_to_clipboard.py
+++ pyui_to_clipboard.py
@@ -1,23 +1,11 @@
 #Copy pyui to clipboard
-"""
-	- Now gets the pyui file of the current file. Can be added as an action.
-	- Error handling if ui file is not associated with current file.
-"""
-
-import clipboard
 import editor
 import re
-
-
 file_path = editor.get_path()
 pattern = "\w+\s*\b*\w+\.py"
 name = re.findall(pattern,file_path)
-
-
 filename= ''.join(name)
 ui_filename = filename+'ui'
-
-
 try:
 	with open(ui_filename) as in_file:
 		clipboard.set(in_file.read())
@@ -25,6 +13,4 @@ try:
 except IOError:
 	print 'The file {} has no associated UI file to add to the clipboard.'.format(filename)
 	print ui_filename
-	
-
 # The regular expression needs work. doesnt work for single letter names.
